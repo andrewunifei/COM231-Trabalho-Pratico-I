d2745991d9aa054597c7058f223a0de50a4ac31e
Luana Freitas <luanacassiaf@gmail.com>
2021-11-11 13:49:46 -0300

    Alterações em query_api

--- Aplicação/api.py
+++ Aplicação/api.py
@@ -116,8 +116,7 @@ def get_review(api_key, business_id):
 
     return request(API_HOST, review_path, api_key)
 
-
-def query_api(location):
+def query_api():
     # O offset são blocos de respostas
     # Se offset = 0 então os SEARCH_LIMIT primeiros registros são retornados
     # Se offset = 20 então os SEARCH_LIMIT + (deslocamento de 20 registros) são retornados
@@ -132,119 +131,124 @@ def query_api(location):
     avaliacoes = list()
     usuarios = list()
     transacoes = list()
+    
+    id_comercios = list()
     id_usuarios = list()
-    
-    response = search(API_KEY, location, offset)
-    total_businesses = response.get('total')
+    id_avaliacoes = list()
 
-    # Passagem dos atributos relevantes do objeto JSON
-    # São necessárias várias requisições, porque a API retorna no máximo 50 objetos
-    # No nosso caso vamos retornar 20 objetos a cada requisição
-    if(total_businesses == 240):
-        number_of_request = response.get('total')/SEARCH_LIMIT # 240/20
+    locations = ['Seattle']
+    #'San Francisco', 'New York CIty', 'Los Angeles', 'Chicago', 
 
-        for request in range(int(number_of_request)):
-            response = search(API_KEY, location, offset)
-            businesses = response.get('businesses')
+    for location in locations:
+        response = search(API_KEY, location, offset)
+        total_businesses = response.get('total')
 
-            if not businesses:
-                print(u'No businesses in {1} found.'.format(location))
-                return
+        # Passagem dos atributos relevantes do objeto JSON
+        # São necessárias várias requisições, porque a API retorna no máximo 50 objetos
+        # No nosso caso vamos retornar 20 objetos a cada requisição
+        if(total_businesses == 240):
+            number_of_request = response.get('total')/SEARCH_LIMIT # 240/20
 
-            for business in businesses:
-                response_comercio = get_business(API_KEY, business['id'])
-                response_avaliacao = get_review(API_KEY, business['id'])
+            for request in range(int(number_of_request)):
+                response = search(API_KEY, location, offset)
+                businesses = response.get('businesses')
 
-                try:
-                    comercios.append(
-                        tabela.Comercio(
-                            id = response_comercio["id"],
-                            nome = response_comercio["name"],
-                            fechado = response_comercio["is_closed"],
-                            telefone = response_comercio["phone"],
-                            preco = response_comercio["price"],
-                            pseudonimo = response_comercio["alias"],
-                            titulo_categoria = response_comercio["categories"][0]["title"],
-                            pseudonimo_categoria = response_comercio["categories"][0]["alias"],
-                            quant_avaliacoes = response_comercio["review_count"]
-                        )
-                    )               
+                if not businesses:
+                    print(u'No businesses in {1} found.'.format(location))
+                    return
 
-                except KeyError as e:
-                    comercios.append(
-                        tabela.Comercio(
-                            id = response_comercio["id"],
-                            nome = response_comercio["name"],
-                            fechado = response_comercio["is_closed"],
-                            telefone = response_comercio["phone"],
-                            preco = None,
-                            pseudonimo = response_comercio["alias"],
-                            titulo_categoria = response_comercio["categories"][0]["title"],
-                            pseudonimo_categoria = response_comercio["categories"][0]["alias"],
-                            quant_avaliacoes = response_comercio["review_count"]
-                        )
-                    )
+                for business in businesses:
+                    response_comercio = get_business(API_KEY, business['id'])
+                    response_avaliacao = get_review(API_KEY, business['id'])
 
-                for type in response_comercio["transactions"]:
-                    transacoes.append(
-                        tabela.Transacao(
-                            id_comercio = response_comercio["id"],
-                            tipo = type
-                        )
-                    )
-
-                localizacoes.append(
-                    tabela.Localizacao(
-                        id_comercio = response_comercio["id"],
-                        # É necessário ver a questão do id serial
-                        cod_postal = response_comercio["location"]["zip_code"],
-                        pais = response_comercio["location"]["country"],
-                        estado = response_comercio["location"]["state"],
-                        cidade = response_comercio["location"]["city"],
-                        logradouro = response_comercio["location"]["address1"]
-                    )
-                )
-
-                for avaliacao in response_avaliacao["reviews"]:
-                    avaliacoes.append(
-                        tabela.Avaliacao(
-                            id_comercio = response_comercio["id"],
-                            id_usuario = avaliacao["user"]["id"],
-                            id = avaliacao["id"],
-                            nota = avaliacao["rating"],
-                            texto = avaliacao["text"],
-                            data = avaliacao["time_created"].split()[0],
-                            horario = avaliacao["time_created"].split()[1]
-                        )
-                    )
-
-                    if avaliacao["user"]["id"] not in id_usuarios:
-                        usuarios.append(
-                                tabela.Usuario(
-                                    id = avaliacao["user"]["id"],
-                                    nome = avaliacao["user"]["name"]
+                    if response_comercio["id"] not in id_comercios:
+                        try:
+                            comercios.append(
+                                tabela.Comercio(
+                                    id = response_comercio["id"],
+                                    nome = response_comercio["name"],
+                                    fechado = response_comercio["is_closed"],
+                                    telefone = response_comercio["phone"],
+                                    preco = response_comercio["price"],
+                                    pseudonimo = response_comercio["alias"],
+                                    titulo_categoria = response_comercio["categories"][0]["title"],
+                                    pseudonimo_categoria = response_comercio["categories"][0]["alias"],
+                                    quant_avaliacoes = response_comercio["review_count"]
                                 )
-                        )
-                        id_usuarios.append(avaliacao["user"]["id"])                    
+                            )               
 
-            offset = offset + 20
-    
-    else:
-        print("A API encontrou mais de 240 casos, precisamos tratar esse cenário")
+                        except KeyError as e:
+                            pass
+
+                    for type in response_comercio["transactions"]:
+                        try:
+                            transacoes.append(
+                                tabela.Transacao(
+                                    id_comercio = response_comercio["id"],
+                                    tipo = type
+                                )
+                            )
+
+                        except KeyError as e:
+                            pass
+
+                    try:
+                        localizacoes.append(
+                            tabela.Localizacao(
+                                id_comercio = response_comercio["id"],
+                                # É necessário ver a questão do id serial
+                                cod_postal = response_comercio["location"]["zip_code"],
+                                pais = response_comercio["location"]["country"],
+                                estado = response_comercio["location"]["state"],
+                                cidade = response_comercio["location"]["city"],
+                                logradouro = response_comercio["location"]["address1"]
+                            )
+                        )
+
+                    except KeyError as e:
+                            pass
+
+                    for avaliacao in response_avaliacao["reviews"]:
+                        try:
+                            if avaliacao["id"] not in id_avaliacoes:
+                                avaliacoes.append(
+                                    tabela.Avaliacao(
+                                        id_comercio = response_comercio["id"],
+                                        id_usuario = avaliacao["user"]["id"],
+                                        id = avaliacao["id"],
+                                        nota = avaliacao["rating"],
+                                        texto = avaliacao["text"],
+                                        data = avaliacao["time_created"].split()[0],
+                                        horario = avaliacao["time_created"].split()[1]
+                                    )
+                                )
+                                id_avaliacoes.append(avaliacao["id"])
+                        
+                        except KeyError as e:
+                            pass
+
+                        try:
+                            if avaliacao["user"]["id"] not in id_usuarios:
+                                usuarios.append(
+                                        tabela.Usuario(
+                                            id = avaliacao["user"]["id"],
+                                            nome = avaliacao["user"]["name"]
+                                        )
+                                )
+                                id_usuarios.append(avaliacao["user"]["id"]) 
+                        except KeyError as e:
+                            pass                   
+
+                offset = offset + 20
+        
+        else:
+            print("A API encontrou mais de 240 casos, precisamos tratar esse cenário")
     
     return comercios, transacoes, localizacoes, avaliacoes, usuarios
 
 def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-l', '--location', dest='location',
-                        default=DEFAULT_LOCATION, type=str,
-                        help='Search location (default: %(default)s)')
-
-    input_values = parser.parse_args()
-
     try:
-        comercios, transacoes, localizacoes, avaliacoes, usuarios = query_api(input_values.location)
+        comercios, transacoes, localizacoes, avaliacoes, usuarios = query_api()
     except HTTPError as error:
         sys.exit(
             'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
--- ddl.sql
+++ ddl.sql
@@ -1,52 +1,52 @@
 CREATE TABLE comercio(
-	id varchar(22) NOT NULL,
-	nome varchar(255) NOT NULL,
-	fechado bool NOT NULL,
-	telefone varchar(20) NOT NULL,
+	id varchar(22),
+	nome varchar(255),
+	fechado bool,
+	telefone varchar(20),
 	preco varchar(4),
 	pseudonimo varchar(255),
-	titulo_categoria varchar(255) NOT NULL,
+	titulo_categoria varchar(255),
 	pseudonimo_categoria varchar(255),
-	quant_avaliacoes int NOT NULL,
+	quant_avaliacoes int,
 	primary key(id)
 );
 
 CREATE TABLE usuario(
-	id varchar(22) NOT NULL,
-	nome varchar(255) NOT NULL,
+	id varchar(22),
+	nome varchar(255),
 	primary key(id)
 );
 
 CREATE TABLE avaliacao(
-	id_comercio varchar(22) NOT NULL,
-	id_usuario varchar(22) NOT NULL,
-	id varchar(22) NOT NULL,
-	nota int NOT NULL,
+	id_comercio varchar(22),
+	id_usuario varchar(22),
+	id varchar(22),
+	nota int,
 	texto text,
-	data date NOT NULL, 
-	horario time NOT NULL,
+	data date, 
+	horario time,
 	primary key(id),
 	foreign key(id_comercio) references comercio(id),
 	foreign key(id_usuario) references usuario(id)
 );
 
 CREATE TABLE transacao(
-	id_comercio varchar(22) NOT NULL,
-	id serial NOT NULL,
-	tipo varchar(255) NOT NULL,
+	id_comercio varchar(22),
+	id serial,
+	tipo varchar(255),
 	primary key(id),
 	foreign key(id_comercio) references comercio(id)
 );
 
 
 CREATE TABLE localizacao(
-	id_comercio varchar(22) NOT NULL,
-	id serial NOT NULL,
-	cod_postal varchar(20) NOT NULL,
-	pais varchar(255) NOT NULL,
-	estado varchar(255) NOT NULL,
-	cidade varchar(255) NOT NULL,
-	logradouro varchar(255) NOT NULL,
+	id_comercio varchar(22),
+	id serial,
+	cod_postal varchar(20),
+	pais varchar(255),
+	estado varchar(255),
+	cidade varchar(255),
+	logradouro varchar(255),
 	primary key(id),
 	foreign key(id_comercio) references comercio(id)
 );
