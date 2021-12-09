993addcf8c3dd3ca3f8b3c9d52b5e19758de7125
Andrew <andrewunifei@gmail.com>
2021-12-08 21:29:06 -0300

    configurações de exibir

---
+++ Adhoc/exibir.py
@@ -0,0 +1,60 @@
+from sqlalchemy import MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from mapeamento import *
+
+def getTable(table_name):
+    if table_name == "Comercio": return Comercio
+    elif table_name == "Usuario": return Usuario
+    elif table_name == "Avaliacao": return Avaliacao
+    elif table_name == "Localizacao": return Localizacao
+    else: return Transacao
+
+def dbCount(session):
+    tables = [Comercio, Usuario, Avaliacao, Localizacao]
+    total = int()
+
+    for table in tables:
+        total += session.query(table).count()
+    
+    return total
+
+def tableCount(table, session):
+    # Função para retornar o número total de registros em uma tabela
+    return session.query(table).count()
+
+
+def tableSize(table_name, wholedb):
+    # Função para consultar o tamanho em bytes de uma tabela e/ou do banco todo
+
+    dburi = "postgresql+psycopg2://postgres:123@localhost:5432/yelp"
+
+    t = dict()
+    m = MetaData(dburi)
+    e = lambda x: m.bind.execute(x).first()[0] # Função anonima que executa a query como um metadado
+    m.reflect()
+    q_pretty_size = "SELECT pg_size_pretty(pg_total_relation_size('%s'))"
+    q_pretty_total = "SELECT pg_size_pretty(pg_database_size('yelp'))"
+
+    # A função pretty retorna o tamanho como "human-readable"
+    t['size'] = e(q_pretty_size % table_name) # Une a string ao table_name
+
+    if wholedb:
+        t['total'] = e(q_pretty_total)
+    
+    return t
+
+def bootloader(table_name, wholedb, criterios):
+    engine = create_engine("postgresql+psycopg2://postgres:123@localhost:5432/yelp", echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    info = {}
+
+    info['tableCount'] = tableCount(getTable(table_name), session)
+    info['dbCount'] = dbCount(session)
+    info['tableSize'] = tableSize(table_name, wholedb)
+
+    session.close()
+
+if __name__ == "__main__":
+    bootloader()
--- Aplicação/DAO.py
+++ Aplicação/DAO.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 class DAOCrud():
     def getSession():
-        engine = create_engine("postgresql+psycopg2://postgres:root@localhost:5432/yelp", echo=True)
+        engine = create_engine("postgresql+psycopg2://postgres:123@localhost:5432/yelp", echo=True)
         Session = sessionmaker(bind=engine)
         session = Session()
         return session
